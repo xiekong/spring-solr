717fbc907d37cd9ef9cbb8f172074895f740ca90
xiekong <345833303@qq.com>
2019-07-16 17:07:32 +0800

    Initial commit

---
+++ src/main/java/com/xiekong/solr/SpringSolrApplication.java
@@ -0,0 +1,13 @@
+package com.xiekong.solr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSolrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSolrApplication.class, args);
+    }
+
+}
